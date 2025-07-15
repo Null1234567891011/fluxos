f493e7970e8006a8b78d61abb2c858c96a19428d
Haecker <120182849+Null1234567891011@users.noreply.github.com>
2025-07-15 06:33:36 +0300

    Update setup.lua

--- setup.lua
+++ setup.lua
@@ -1,1 +1,7 @@
-echo привет!
+component = require("component")
+gpu = component.gpu
+
+w, h = gpu.getResolution()
+gpu.setBackground(0xFFFFFF)
+gpu.setForeground(0x000000)
+gpu.fill(1, 1, w, h, " ")
